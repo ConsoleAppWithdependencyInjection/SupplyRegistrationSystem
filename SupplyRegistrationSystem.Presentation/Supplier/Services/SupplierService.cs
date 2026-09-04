0bcc5a361101ffafa9eb23478bfbf9a214ef91d1
Ali Peida <din GitHub-email>
2026-09-04 14:30:02 +0200

    Add supplier service

---
+++ SupplyRegistrationSystem.Presentation/Supplier/Services/SupplierService.cs
@@ -0,0 +1,13 @@
+﻿using SupplyRegistrationSystem.Presentation.Supplier.Models;
+
+namespace SupplyRegistrationSystem.Presentation.Supplier.Services;
+
+public class SupplierService : ISupplierService
+{
+    private readonly List<SupplierModel> _suppliers = [];
+
+    public void AddSupplier(SupplierModel supplier)
+    {
+        _suppliers.Add(supplier);
+    }
+}
