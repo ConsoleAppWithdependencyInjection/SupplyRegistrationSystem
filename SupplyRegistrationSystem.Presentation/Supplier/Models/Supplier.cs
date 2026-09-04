a4f9ff4973ca4cdd27abb896d3adaabdb52da146
Ali Peida <din GitHub-email>
2026-09-04 14:03:17 +0200

    Add supplier model

--- SupplyRegistrationSystem.Presentation/Supplier/Models/Supplier.cs
+++ SupplyRegistrationSystem.Presentation/Supplier/Models/Supplier.cs
@@ -2,5 +2,8 @@
 
 public class Supplier
 {
-
+    public string CompanyName {get; set;} = "";
+    public string PhoneNumber {get; set;} = "";
+    public string BillingAddress {get; set;}= "";
+    public string OrganizationNumber { get; set; } = "";
 }
