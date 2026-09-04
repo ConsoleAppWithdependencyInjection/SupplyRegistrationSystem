276b8e653da63ec7d12c30aa4dba409a85323f3e
Ali Peida <din GitHub-email>
2026-09-04 14:59:55 +0200

    Add supplier dialog service

---
+++ SupplyRegistrationSystem.Presentation/Supplier/Dialogs/SupplierDialogService.cs
@@ -0,0 +1,45 @@
+﻿using SupplyRegistrationSystem.Presentation.Supplier.Models;
+using SupplyRegistrationSystem.Presentation.Supplier.Services;
+
+namespace SupplyRegistrationSystem.Presentation.Supplier.Dialogs;
+
+public class SupplierDialogService : ISupplierDialog
+{
+    private readonly ISupplierService _supplierService;
+
+    public SupplierDialogService(ISupplierService supplierService)
+    {
+        _supplierService = supplierService;
+    }
+
+    public void AddSupplier()
+    {
+        Console.Write("Ange företagsnamn: ");
+        string companyName = Console.ReadLine() ?? "";
+
+        Console.Write("Ange telefonnummer: ");
+        string phoneNumber = Console.ReadLine() ?? "";
+
+        Console.Write("Ange faktureringsadress: ");
+        string billingAddress = Console.ReadLine() ?? "";
+
+        Console.Write("Ange organisationsnummer: ");
+        string organizationNumber = Console.ReadLine() ?? "";
+
+        SupplierModel supplier = new()
+        {
+            CompanyName = companyName,
+            PhoneNumber = phoneNumber,
+            BillingAddress = billingAddress,
+            OrganizationNumber = organizationNumber
+        };
+
+        _supplierService.AddSupplier(supplier);
+
+        Console.WriteLine();
+        Console.WriteLine($"Leverantören {supplier.CompanyName} har lagts till.");
+        Console.WriteLine($"Telefonnummer: {supplier.PhoneNumber}");
+        Console.WriteLine($"Faktureringsadress: {supplier.BillingAddress}");
+        Console.WriteLine($"Organisationsnummer: {supplier.OrganizationNumber}");
+    }
+}
